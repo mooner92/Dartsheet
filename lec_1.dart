9a317939b8f4572f93481a5dae5b8a4581f83c79
Mooner92 <chlaudgjs0989@gmail.com>
2023-01-02 14:18:22 +0900

    add sheet1

---
+++ lec_1.dart
@@ -0,0 +1,38 @@
+void main() {
+  //print('hello factory');
+  var name = '코드팩토리';
+  print(name);
+  var name2 = '레드벨벳';
+  print(name2);
+
+  name = '플러터 프로그래밍';
+  print(name);
+  int number1 = 10;
+  print(number1);
+  int number2 = 15;
+  print(number2);
+  int number3 = -20;
+  print(number3);
+  print(number1 + number2);
+  print(number1 - number2);
+  print(number1 / number2);
+  print(number1 * number2);
+  double num1 = 2.5;
+  double num2 = 0.5;
+  print(num1 + num2);
+  print(num1 - num2);
+  print(num1 / num2);
+  print(num1 * num2);
+  bool isTrue = true;
+  bool isFalse = false;
+  print(isTrue);
+  print(isFalse);
+  String s1 = '레드벨벳';
+  String s2 = '코드팩토리';
+  print(s1 + s2);
+  print(name2.runtimeType);
+  String s3 = '슬기';
+  print(s1 + ' ' + s3);
+  print('${name} ${name2}');
+  print('$s3 $s2');
+}
